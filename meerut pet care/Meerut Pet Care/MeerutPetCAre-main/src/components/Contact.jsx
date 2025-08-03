6bea5eb936607a114e9b5bb7ed1dad68d59f71fe
OB275 <omkarbhoir2727@gmail.com>
2025-08-04 00:18:01 +0530

    Add files via upload

---
+++ meerut pet care/Meerut Pet Care/MeerutPetCAre-main/src/components/Contact.jsx
@@ -0,0 +1,16 @@
+import Map from "./Map";
+import React from "react";
+import Address from "./Address";
+import BookAService from "./BookAService";
+
+const Contact = () => {
+  return (
+    <>
+      <Map />
+      <Address />
+      <BookAService />
+    </>
+  );
+};
+
+export default Contact;
